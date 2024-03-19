85450ca546b90790a028f0b38ab66414afa64757
DHVS-Based <diego@based.so>
2024-03-19 16:30:00 -0600

    Create event type

--- apps/web/pages/based/index.tsx
+++ apps/web/pages/based/index.tsx
@@ -1,3 +1,4 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import type { AvailabilityFormValues } from "@pages/availability/[schedule]";
 import { AvailabilityForm } from "@pages/availability/[schedule]";
 import { useBased, type ExposedComponent, type ExposedFunction } from "@pages/based/based.js/Based";
@@ -12,18 +13,30 @@ import { Responsive, WidthProvider } from "react-grid-layout";
 import "react-grid-layout/css/styles.css";
 import { useForm } from "react-hook-form";
 import "react-resizable/css/styles.css";
+import type { z } from "zod";
 
 import type { Dayjs } from "@calcom/dayjs";
 import dayjs from "@calcom/dayjs";
 import { useFilterQuery } from "@calcom/features/bookings/lib/useFilterQuery";
+import { useOrgBranding } from "@calcom/features/ee/organizations/context/provider";
+import { getTeamsFiltersFromQuery } from "@calcom/features/filters/lib/getTeamsFiltersFromQuery";
 import { BookingStatusLineChart } from "@calcom/features/insights/components";
 import { FiltersProvider } from "@calcom/features/insights/context/FiltersProvider";
 import { Filters } from "@calcom/features/insights/filters";
 import Shell from "@calcom/features/shell/Shell";
+import { classNames } from "@calcom/lib";
+import { HOSTED_CAL_FEATURES } from "@calcom/lib/constants";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
+import { useRouterQuery } from "@calcom/lib/hooks/useRouterQuery";
 import { HttpError } from "@calcom/lib/http-error";
+import { md } from "@calcom/lib/markdownIt";
+import slugify from "@calcom/lib/slugify";
+import turndown from "@calcom/lib/turndownService";
+import { MembershipRole, SchedulingType } from "@calcom/prisma/enums";
+import { unlockedManagedEventTypeProps } from "@calcom/prisma/zod-utils";
+import { createEventTypeInput } from "@calcom/prisma/zod/custom/eventtype";
 import { trpc } from "@calcom/trpc/react";
-import { Button, showToast } from "@calcom/ui";
+import { Alert, Button, Editor, Form, TextField, showToast, RadioGroup as RadioArea } from "@calcom/ui";
 
 import useMeQuery from "@lib/hooks/useMeQuery";
 
@@ -88,11 +101,12 @@ function Main() {
             onClick={() =>
               generateUI(
                 !thread || !thread.messages
-                  ? "Reschedule all Friday Bookings. Please explain that I catch the flu and I will not be able to make it :,c"
+                  ? "Create a new event type to onboard users into the streamers dashboard of my Twitch extension called Qapla, regularly an onboarding lasts around 45 minutes"
                   : "Show me the insights please <3!"
               )
             }>
-            {!thread || !thread.messages ? "Reschedule Friday Bookings" : "Load Insights"}
+            {/* ? "Reschedule all Friday Bookings. Please explain that I catch the flu and I will not be able to make it :,c" */}
+            {!thread || !thread.messages ? "Create event type" : "Load Insights"}
           </Button>
         }>
         <ResponsiveGridLayout
@@ -337,6 +351,272 @@ const RescheduleComponent = ({ day, reason }: { day: Dayjs; reason: string }) =>
   );
 };
 
+const CreateEventType = ({
+  title,
+  description,
+  duration,
+}: {
+  title?: string;
+  description?: string;
+  duration?: number;
+}) => {
+  const routerQuery = useRouterQuery();
+  const filters = getTeamsFiltersFromQuery(routerQuery);
+  const { data } = trpc.viewer.eventTypes.getByViewer.useQuery(filters && { filters }, {
+    refetchOnWindowFocus: false,
+    gcTime: 1 * 60 * 60 * 1000,
+    staleTime: 1 * 60 * 60 * 1000,
+  });
+
+  const { data: user } = useMeQuery();
+
+  const utils = trpc.useContext();
+  const { t } = useLocale();
+  const [firstRender, setFirstRender] = useState(true);
+  const orgBranding = useOrgBranding();
+
+  const teamId = null;
+  const pageSlug = user?.username;
+
+  const isOrganization = !!user?.organizationId;
+
+  let profileOptions = null;
+  let teamProfile = null;
+
+  if (data) {
+    profileOptions = data.profiles
+      .filter((profile) => !profile.readOnly)
+      .map((profile) => {
+        return {
+          teamId: profile.teamId,
+          label: profile.name || profile.slug,
+          image: profile.image,
+          membershipRole: profile.membershipRole,
+          slug: profile.slug,
+        };
+      });
+
+    teamProfile = profileOptions.find((profile) => profile.teamId === teamId);
+  }
+
+  const isSelfHosted = !HOSTED_CAL_FEATURES;
+  const isEE = !!(isSelfHosted || isOrganization);
+  const form = useForm<z.infer<typeof createEventTypeInput>>({
+    defaultValues: {
+      length: 15,
+    },
+    resolver: zodResolver(createEventTypeInput),
+  });
+
+  const schedulingTypeWatch = form.watch("schedulingType");
+  const isManagedEventType = schedulingTypeWatch === SchedulingType.MANAGED;
+
+  useEffect(() => {
+    if (isManagedEventType) {
+      form.setValue("metadata.managedEventConfig.unlockedFields", unlockedManagedEventTypeProps);
+    } else {
+      form.setValue("metadata", null);
+    }
+  }, [schedulingTypeWatch]);
+
+  useEffect(() => {
+    if (title) {
+      form.setValue("title", title);
+      if (form.formState.touchedFields["slug"] === undefined) {
+        form.setValue("slug", slugify(title));
+      }
+    }
+
+    if (description) {
+      form.setValue("description", turndown(description));
+    }
+
+    if (duration) {
+      form.setValue("length", duration);
+    }
+  }, [title, description, form, duration]);
+
+  const { register } = form;
+
+  const isAdmin =
+    teamId !== undefined &&
+    (teamProfile?.membershipRole === MembershipRole.OWNER ||
+      teamProfile?.membershipRole === MembershipRole.ADMIN);
+
+  const createMutation = trpc.viewer.eventTypes.create.useMutation({
+    onSuccess: async ({ eventType }) => {
+      await utils.viewer.eventTypes.getByViewer.invalidate();
+      showToast(
+        t("event_type_created_successfully", {
+          eventTypeTitle: eventType.title,
+        }),
+        "success"
+      );
+      form.reset();
+    },
+    onError: (err) => {
+      if (err instanceof HttpError) {
+        const message = `${err.statusCode}: ${err.message}`;
+        showToast(message, "error");
+      }
+
+      if (err.data?.code === "BAD_REQUEST") {
+        const message = `${err.data.code}: ${t("error_event_type_url_duplicate")}`;
+        showToast(message, "error");
+      }
+
+      if (err.data?.code === "UNAUTHORIZED") {
+        const message = `${err.data.code}: ${t("error_event_type_unauthorized_create")}`;
+        showToast(message, "error");
+      }
+    },
+  });
+
+  const urlPrefix = orgBranding?.fullDomain ?? process.env.NEXT_PUBLIC_WEBSITE_URL;
+
+  if (!data) return null;
+
+  return (
+    <Form
+      form={form}
+      handleSubmit={(values) => {
+        createMutation.mutate(values);
+      }}>
+      <div className="mt-3 space-y-6 pb-11">
+        {teamId && (
+          <TextField
+            type="hidden"
+            labelProps={{ style: { display: "none" } }}
+            {...register("teamId", { valueAsNumber: true })}
+            value={teamId}
+          />
+        )}
+        <TextField
+          label={t("title")}
+          placeholder={t("quick_chat")}
+          data-testid="event-type-quick-chat"
+          {...register("title")}
+          onChange={(e) => {
+            form.setValue("title", e?.target.value);
+            if (form.formState.touchedFields["slug"] === undefined) {
+              form.setValue("slug", slugify(e?.target.value));
+            }
+          }}
+        />
+
+        {urlPrefix && urlPrefix.length >= 21 ? (
+          <div>
+            <TextField
+              label={`${t("url")}: ${urlPrefix}`}
+              required
+              addOnLeading={<>/{!isManagedEventType ? pageSlug : t("username_placeholder")}/</>}
+              {...register("slug")}
+              onChange={(e) => {
+                form.setValue("slug", slugify(e?.target.value), { shouldTouch: true });
+              }}
+            />
+
+            {isManagedEventType && (
+              <p className="mt-2 text-sm text-gray-600">{t("managed_event_url_clarification")}</p>
+            )}
+          </div>
+        ) : (
+          <div>
+            <TextField
+              label={t("url")}
+              required
+              addOnLeading={
+                <>
+                  {urlPrefix}/{!isManagedEventType ? pageSlug : t("username_placeholder")}/
+                </>
+              }
+              {...register("slug")}
+            />
+            {isManagedEventType && (
+              <p className="mt-2 text-sm text-gray-600">{t("managed_event_url_clarification")}</p>
+            )}
+          </div>
+        )}
+        {!teamId && (
+          <>
+            <Editor
+              getText={() => md.render(form.getValues("description") || "")}
+              setText={(value: string) => form.setValue("description", turndown(value))}
+              excludedToolbarItems={["blockType", "link"]}
+              placeholder={t("quick_video_meeting")}
+              firstRender={firstRender}
+              setFirstRender={setFirstRender}
+            />
+
+            <div className="relative">
+              <TextField
+                type="number"
+                required
+                min="10"
+                placeholder="15"
+                label={t("duration")}
+                className="pr-4"
+                {...register("length", { valueAsNumber: true })}
+                addOnSuffix={t("minutes")}
+              />
+            </div>
+          </>
+        )}
+
+        {teamId && (
+          <div className="mb-4">
+            <label htmlFor="schedulingType" className="text-default block text-sm font-bold">
+              {t("assignment")}
+            </label>
+            {form.formState.errors.schedulingType && (
+              <Alert
+                className="mt-1"
+                severity="error"
+                message={form.formState.errors.schedulingType.message}
+              />
+            )}
+            <RadioArea.Group
+              onValueChange={(val: SchedulingType) => {
+                form.setValue("schedulingType", val);
+              }}
+              className={classNames("mt-1 flex gap-4", isAdmin && "flex-col")}>
+              <RadioArea.Item
+                {...register("schedulingType")}
+                value={SchedulingType.COLLECTIVE}
+                className={classNames("w-full text-sm", !isAdmin && "w-1/2")}
+                classNames={{ container: classNames(isAdmin && "w-full") }}>
+                <strong className="mb-1 block">{t("collective")}</strong>
+                <p>{t("collective_description")}</p>
+              </RadioArea.Item>
+              <RadioArea.Item
+                {...register("schedulingType")}
+                value={SchedulingType.ROUND_ROBIN}
+                className={classNames("text-sm", !isAdmin && "w-1/2")}
+                classNames={{ container: classNames(isAdmin && "w-full") }}>
+                <strong className="mb-1 block">{t("round_robin")}</strong>
+                <p>{t("round_robin_description")}</p>
+              </RadioArea.Item>
+              <>
+                {isAdmin && isEE && (
+                  <RadioArea.Item
+                    {...register("schedulingType")}
+                    value={SchedulingType.MANAGED}
+                    className={classNames("text-sm", !isAdmin && "w-1/2")}
+                    classNames={{ container: classNames(isAdmin && "w-full") }}
+                    data-testid="managed-event-type">
+                    <strong className="mb-1 block">{t("managed_event")}</strong>
+                    <p>{t("managed_event_description")}</p>
+                  </RadioArea.Item>
+                )}
+              </>
+            </RadioArea.Group>
+          </div>
+        )}
+      </div>
+    </Form>
+  );
+};
+
 const availableComponents: ExposedComponent[] = [
   {
     name: "Bookings list",
@@ -393,6 +673,36 @@ const availableComponents: ExposedComponent[] = [
     },
     extraData: {
       height: 18,
+      width: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
+    },
+  },
+  {
+    name: "Create event type",
+    description: "Shows an, optionally, pre-filled form that allows the user to create a new event type",
+    element: CreateEventType,
+    props: {
+      title: {
+        type: "string",
+        description: "Title for the new event type",
+      },
+      description: {
+        type: "string",
+        description: "Description for the new event type",
+      },
+      duration: {
+        type: "number",
+        description: "Duration (in minutes) for the event type",
+      },
+    },
+    loader: ({ title, description, duration }) => {
+      return {
+        title,
+        description,
+        duration,
+      };
+    },
+    extraData: {
+      height: 12,
       width: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
     },
   },
